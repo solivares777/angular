bdeebd9de759ae5fdd34cc859de8fb412dbc2bc2
solivares777 <salvador.olivares.sv@gmail.com>
2025-07-23 20:27:31 -0600

    Mi primer commit

---
+++ modelo/renta.spec.ts
@@ -0,0 +1,7 @@
+import { Renta } from './renta';
+
+describe('Renta', () => {
+  it('should create an instance', () => {
+    expect(new Renta()).toBeTruthy();
+  });
+});
---
+++ modelo/renta.ts
@@ -0,0 +1,10 @@
+export class Renta {
+    noNit: String=""; 
+    contribuyente:String=""; 
+    salarioMensual: number=0;
+    
+    isss: number=0;
+    afp: number=0;
+    calculoRenta: number=0; 
+    salarioLiquido: number=0;
+}
